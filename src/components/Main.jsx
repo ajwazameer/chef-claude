2038101bb486689fb6c46bb345cc2dd6944e0301
ajwazameer <ajwazameer2004@gmail.com>
2026-07-25 21:21:18 +0500

    Created an ingredient input UI

---
+++ src/components/Main.jsx
@@ -0,0 +1,14 @@
+export default function Main() {
+  return (
+    <main>
+      <form action="">
+        <input
+          type="text"
+          aria-label="Add ingredients"
+          placeholder="e.g. oregano"
+        />
+        <button>+ Add ingredients</button>
+      </form>
+    </main>
+  );
+}
