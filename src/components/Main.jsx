2022fe50b8d30c1817593a6837aa1f8ddd2c8983
ajwazameer <ajwazameer2004@gmail.com>
2026-07-26 13:31:55 +0500

    Used STATES to modify Ingredient Lists and Render on screen

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,14 +1,33 @@
+import { useState } from "react";
 export default function Main() {
+  const [ingredients, setIngredients] = useState([
+    "Chicken",
+    "Oregano",
+    "Tomatoes",
+  ]);
+  const ingrdientList = ingredients.map((ingr, index) => {
+    return <li key={index}>{ingr}</li>;
+  });
+  const onSubmitMessage = (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget);
+    let newIngredient = formData.get("ingredient");
+    setIngredients((prevList) => {
+      return [...prevList, newIngredient];
+    });
+  };
   return (
     <main>
-      <form action="">
+      <form onSubmit={onSubmitMessage}>
         <input
           type="text"
           aria-label="Add ingredients"
           placeholder="e.g. oregano"
+          name="ingredient"
         />
         <button>+ Add ingredients</button>
       </form>
+      <ul className="ingr-list">{ingrdientList}</ul>
     </main>
   );
 }
