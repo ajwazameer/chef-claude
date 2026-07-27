eb0ab530d9c6a36f76c785a9c7782b3d388a4a26
ajwazameer <ajwazameer2004@gmail.com>
2026-07-27 23:11:28 +0500

    Created Components

---
+++ src/components/IngredientsList.jsx
@@ -0,0 +1,22 @@
+export default function IngredientsList(props) {
+  const ingredientList = props.ingredients.map((ingr, index) => {
+    return <li key={index}>{ingr}</li>;
+  });
+  return (
+    <section>
+      <div className="ingr-section">
+        <h1>Ingredients on hand:</h1>
+        <ul className="ingr-list">{ingredientList}</ul>
+      </div>
+      {props.ingredients.length > 3 && (
+        <div className="recipe-section">
+          <div className="recipe-div">
+            <h3>Ready for a recipe?</h3>
+            <span>Generate a recipe from your list of ingredients.</span>
+          </div>
+          <button onClick={props.showRecipe}>Get a recipe</button>
+        </div>
+      )}
+    </section>
+  );
+}
